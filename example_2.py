44f56ab5610bdbecdc56164334c5a55f6bee654e
greydoubt <43443470+greydoubt@users.noreply.github.com>
2024-01-11 19:33:21 -0800

    Create example_2.py

---
+++ example_2.py
@@ -0,0 +1,23 @@
+import omni.ext
+import omni.ui as ui
+
+# Any class derived from `omni.ext.IExt` in top level module (defined in `python.modules` of `extension.toml`) will be
+# instantiated when extension gets enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
+# on_shutdown() is called.
+class MyExtension(omni.ext.IExt):
+    # ext_id is current extension id. It can be used with extension manager to query additional information, like where
+    # this extension is located on filesystem.
+    def on_startup(self, ext_id):
+        print("[omni.example.spawn_prims] MyExtension startup")
+
+        self._window = ui.Window("Spawn Primitives", width=300, height=300)
+        with self._window.frame:
+            with ui.VStack():
+
+                def on_click():
+                    print("clicked!")
+
+                ui.Button("Spawn Cube", clicked_fn=lambda: on_click())
+
+    def on_shutdown(self):
+        print("[omni.example.spawn_prims] MyExtension shutdown")
